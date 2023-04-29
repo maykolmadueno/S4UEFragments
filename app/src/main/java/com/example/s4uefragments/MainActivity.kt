76fddd7e7e4b7f9a9cfca0aacef2c02bc9f8454c
Maykol <20100843@ue.edu.pe>
2023-04-29 09:50:38 -0500

    Se agrego nuevos fragmentos

--- app/src/main/java/com/example/s4uefragments/MainActivity.kt
+++ app/src/main/java/com/example/s4uefragments/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navprincipal, R.id.navregistro, R.id.navmusica
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
